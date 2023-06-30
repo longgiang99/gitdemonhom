743afee8d9d07403bc6297deab3630cfb39948f7
MaiHoSyDat <maihosydat@gmail.com>
2023-06-30 09:26:36 +0700

    dat

---
+++ src/main/java/demoGitNhom/GitNhom.java
@@ -0,0 +1,32 @@
+package demoGitNhom;
+
+public class GitNhom {
+    int id;
+    String name;
+
+    public GitNhom(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public GitNhom() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
